a00c2b26657d98ac70242355bbf447e8a2ecba2a
brownrice55 <113149599+brownrice55@users.noreply.github.com>
2025-08-23 17:12:53 +0900

    feat: add a feature to display lists that aren't completed yet

--- react/src/components/settings/FormSettings0.tsx
+++ react/src/components/settings/FormSettings0.tsx
@@ -24,7 +24,9 @@ export default function FormSettings0({
   onUpdate,
 }: FormSettings0Props) {
   const originalData = getData();
-  const data = originalData ? originalData : new Map();
+  const [data, setData] = useState<Map<number, Inputs>>(
+    originalData ? originalData : new Map()
+  );
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -52,7 +54,17 @@ export default function FormSettings0({
 
   const [isCustom, setIsCustom] = useState<boolean>(false);
 
-  const currentDataValue = keyNumber ? data.get(keyNumber) : {};
+  const currentDataValue: Inputs | undefined = keyNumber
+    ? data.get(keyNumber)
+    : {
+        goal: "",
+        goals: [""],
+        status: 0,
+        date: "",
+        hasPeriod: "",
+        period: "",
+        customDate: "",
+      };
   const defaultValues = {
     goal: keyNumber ? currentDataValue?.goal : "",
     period: keyNumber ? currentDataValue?.period : "m1",
@@ -150,6 +162,20 @@ export default function FormSettings0({
     const newClassName =
       e.target.value === "true" ? "mt-3 mb-4" : "mt-3 mb-4 checkboxDisabled";
     setPeriodClassName(newClassName);
+  };
+
+  const handleDelete = (aKey: number) => {
+    const newData = new Map(data);
+    newData.delete(aKey);
+    localStorage.setItem("goalManagement", JSON.stringify([...newData]));
+    setData(newData);
+  };
+
+  const handleEdit = (aKey: number) => {
+    const currentDataValue = data.get(aKey);
+    if (currentDataValue) {
+      onUpdate(currentDataValue.status, aKey);
+    }
   };
 
   useEffect(() => {
@@ -292,6 +318,49 @@ export default function FormSettings0({
           </Button>
         </div>
       </Form>
+      {data && data.size ? (
+        <>
+          <div className="border-top py-2 mt-5">
+            <p className="py-3">登録済みの達成したいことリスト</p>
+            <div className="js-goalList"></div>
+          </div>
+          {[...data].map(([key, val]) => (
+            <Row key={key}>
+              <Col sm={10}>{val.goal}</Col>
+              <Col sm={1}>
+                <Button variant="light" onClick={() => handleEdit(key)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    className="bi bi-pencil"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325" />
+                  </svg>
+                </Button>
+              </Col>
+              <Col sm={1}>
+                <Button variant="light" onClick={() => handleDelete(key)}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    className="bi bi-trash3"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                  </svg>
+                </Button>
+              </Col>
+            </Row>
+          ))}
+        </>
+      ) : (
+        ""
+      )}
     </>
   );
 }
